c5d580e9e44962c6748a79bb70ae88e8db00c503
Isaias Medina <isamed92@gmail.com>
2023-07-24 11:34:50 -0300

    update

--- lib/presentation/providers/permissions/permissions_provider.dart
+++ lib/presentation/providers/permissions/permissions_provider.dart
@@ -1,6 +1,39 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 //state notifier provider
+
+class PermissionsNotifier extends StateNotifier<PermissionState> {
+  PermissionsNotifier() : super(PermissionState()) {
+    checkPermissions();
+  }
+
+  Future<void> checkPermissions() async {
+    final permissionsList = await Future.wait([
+      Permission.camera.status,
+      Permission.photos.status,
+      Permission.sensors.status,
+      Permission.location.status,
+      Permission.locationAlways.status,
+      Permission.locationWhenInUse.status,
+    ]);
+
+    state = state.copyWith(
+      camera: permissionsList[0],
+      photoLibrary: permissionsList[1],
+      sensors: permissionsList[2],
+      location: permissionsList[3],
+      locationAlways: permissionsList[4],
+      locationWhenInUse: permissionsList[5],
+    );
+  }
+
+  requestCameraAccess() async {
+    final status = await Permission.camera.request();
+
+    if (status.isGranted) {}
+  }
+}
 
 class PermissionState {
   final PermissionStatus camera;
