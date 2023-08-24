313ba33d8177dd3ffee998aaf7fbbc239637d7d8
Isaias Medina <isamed92@gmail.com>
2023-08-24 01:26:11 -0300

    gyroscope screen added

--- lib/presentation/screens/sensors/gyroscope_screen.dart
+++ lib/presentation/screens/sensors/gyroscope_screen.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class GyroscopeScreen extends StatelessWidget {
+import '../../providers/providers.dart';
+
+class GyroscopeScreen extends ConsumerWidget {
   const GyroscopeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final gyroscope$ = ref.watch(gyrosopeProvider);
     return Scaffold(
       appBar: AppBar(title: const Text('Giroscopio')),
-      body: const Center(child: Text('GyroscopeScreen')),
+      body: Center(
+          child: gyroscope$.when(
+              data: (value) =>
+                  Text(value.toString(), style: const TextStyle(fontSize: 30)),
+              error: (error, stackTrace) => Text('$error'),
+              loading: () => const CircularProgressIndicator())),
     );
   }
 }
